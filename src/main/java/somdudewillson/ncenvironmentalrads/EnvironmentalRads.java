0e624db685bcd53eb5fa8d975bc8f41098f89346
Somdudewillson <somdudewilson@gmail.com>
2020-07-24 10:56:15 -0700

    Incremented version number.

--- src/main/java/somdudewillson/ncenvironmentalrads/EnvironmentalRads.java
+++ src/main/java/somdudewillson/ncenvironmentalrads/EnvironmentalRads.java
@@ -22,7 +22,7 @@ public class EnvironmentalRads
 {
     public static final String MODID = "ncenvironmentalrads";
     public static final String NAME = "Environmental Rads : NuclearCraft Addon";
-    public static final String VERSION = "1.12.2-1.0.5.0";
+    public static final String VERSION = "1.12.2-1.0.5.1";
     /* MCVERSION-MAJORMOD.MAJORAPI.MINOR.PATCH
      * 		MCVERSION
 	 * Always matches the Minecraft version the mod is for.
